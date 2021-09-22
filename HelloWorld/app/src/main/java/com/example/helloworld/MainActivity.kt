a832930b9bc3fdccb5e745ed2689e3d9f45f1cad
rickrribeiro <rickribeiro99@gmail.com>
2021-09-22 01:15:37 -0300

    atv1

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext,"OnCreate", Toast.LENGTH_SHORT).show()
+
+    }
+    override fun onStart() {
+        super.onStart()
+        setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext,"OnCreate", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext,"OnResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext,"OnPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(applicationContext,"OnStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext,"OnDestroy", Toast.LENGTH_SHORT).show()
     }
 }
