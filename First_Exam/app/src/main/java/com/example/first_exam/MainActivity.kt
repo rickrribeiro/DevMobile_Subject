507cc4db558a699cb9f5bcea5b8464509b0dba7b
rickrribeiro <rickribeiro99@gmail.com>
2021-09-24 02:50:24 -0300

    login funcional, bloqueando com tentativas(comentado), sharedPreferences

--- First_Exam/app/src/main/java/com/example/first_exam/MainActivity.kt
+++ First_Exam/app/src/main/java/com/example/first_exam/MainActivity.kt
@@ -1,11 +1,85 @@
 package com.example.first_exam
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var username:String = "";private set;
+    var password:String = "";private set;
+    var attempts:Int = 0;private set;
+    var sharedPreferences: SharedPreferences? = null;private set;
+    ////// fazer uma com updateAttempts
+    private fun updateUsernameAndPassword(username:String, password: String){
+        var sharedPreferencesEditor = sharedPreferences?.edit() as SharedPreferences.Editor
+        if(sharedPreferencesEditor != null){
+            sharedPreferencesEditor.putString("username", username)
+            sharedPreferencesEditor.putString("password", password)
+            sharedPreferencesEditor.commit()
+        }
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+     
+        
+        username = sharedPreferences!!.getString("username", "").toString()
+        password = sharedPreferences!!.getString("password", "").toString()
+        if(username=="" || password==""){
+            username="admin"
+            password="admin"
+            updateUsernameAndPassword(username, password)
+        }
+        var btnLogin =  findViewById(R.id.btnLogin) as Button
+        btnLogin.setOnClickListener(this)
+        
     }
+
+    override fun onClick(p0: View?) {
+        
+        if (p0!!.id==R.id.btnLogin){
+            var inputUsername = findViewById<EditText>(R.id.inputUsername);
+            var inputPassword = findViewById(R.id.inputPassword) as EditText;
+           
+            if(inputUsername.text.toString()==""){
+                Toast.makeText(this, getString(R.string.username_missing), Toast.LENGTH_LONG).show()
+            }else if(inputPassword.text.toString()==""){
+                 Toast.makeText(this, getString(R.string.password_missing), Toast.LENGTH_LONG).show()
+            }else{
+                if(inputUsername.text.toString() == username && inputPassword.text.toString() == password){
+                    var sharedPreferencesEditor = sharedPreferences?.edit() as SharedPreferences.Editor
+                    if(sharedPreferencesEditor != null){
+                        sharedPreferencesEditor.putInt("attempts", 0)
+                        sharedPreferencesEditor.commit()
+                    }
+                    var intent = Intent(this, MainPage::class.java)
+                    startActivity(intent)
+                }else{
+
+                    attempts = sharedPreferences!!.getInt("attempts", 0) +1
+                    var sharedPreferencesEditor = sharedPreferences?.edit() as SharedPreferences.Editor
+                    if(sharedPreferencesEditor != null){
+                        sharedPreferencesEditor.putInt("attempts", attempts)
+                        sharedPreferencesEditor.commit()
+                    }
+                    Toast.makeText(this, getString(R.string.wrong_password_1)+(3-attempts)+getString(R.string.wrong_password_2), Toast.LENGTH_LONG).show()    
+                }
+            }
+        }
+        // if xxxxxxx do_it else do_it
+        // if (p0.id == R.id.button_about){
+        //     var intent = Intent(this, AboutActivity::class.java)
+        //     startActivity(intent)
+        // }
+        
+    }
+
+
 }
--- First_Exam/app/src/main/java/com/example/first_exam/SplashScreen.kt
+++ First_Exam/app/src/main/java/com/example/first_exam/SplashScreen.kt
@@ -1,9 +1,11 @@
 package com.example.first_exam
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
@@ -11,20 +13,23 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
+        var sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        var attempts = sharedPreferences!!.getInt("attempts", 0)
+        if(attempts>=3){
+            /////////// SUBSTITUIR ISSO POR FECHAR A APLICAÇÃO E LEMBRAR DE USAR STRING
+            Toast.makeText(this, "NÃO FOI POSSIVEL RODAR", Toast.LENGTH_LONG).show()
+        }
+        
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
+        
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 5000) // 3000 is the delayed time in milliseconds.
+        }, 5000) 
     }
 }
