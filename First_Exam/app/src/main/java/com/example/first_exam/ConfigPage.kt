565a7c4a9f54c6c0fa6d1bde3c7da3d694a4d996
rickrribeiro <rickribeiro99@gmail.com>
2021-09-24 12:20:51 -0300

    config page

---
+++ First_Exam/app/src/main/java/com/example/first_exam/ConfigPage.kt
@@ -0,0 +1,11 @@
+package com.example.first_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config_page)
+    }
+}
--- First_Exam/app/src/main/java/com/example/first_exam/MainPage.kt
+++ First_Exam/app/src/main/java/com/example/first_exam/MainPage.kt
@@ -1,11 +1,22 @@
 package com.example.first_exam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
-class MainPage : AppCompatActivity() {
+class MainPage : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+        var btnConfig = findViewById(R.id.btnConfig) as ImageButton
+        btnConfig.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, ConfigPage::class.java)
+        startActivity(intent)
     }
 }
