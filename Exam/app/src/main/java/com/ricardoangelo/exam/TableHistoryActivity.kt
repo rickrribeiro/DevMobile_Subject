777dd9ef9f95d3b0294514b8f7603807ebeab962
rickrribeiro <rickribeiro99@gmail.com>
2021-12-04 23:22:00 -0300

    row, pegando dados do firebase e tentando montar tablea

--- Exam/app/src/main/java/com/ricardoangelo/exam/TableHistoryActivity.kt
+++ Exam/app/src/main/java/com/ricardoangelo/exam/TableHistoryActivity.kt
@@ -1,11 +1,34 @@
 package com.ricardoangelo.exam
-
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.Toast
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class TableHistoryActivity : AppCompatActivity() {
+    var database = Firebase.database
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table_history)
+
+        setUpTableData()
+    }
+
+    private fun setUpTableData() {
+        val myRef = database.getReference("location_history")
+        val table = findViewById<TableLayout>(R.id.location_table); // .orderByChild("starCount")
+        val locationData = myRef.get().addOnSuccessListener {
+        Toast.makeText(this, it.value.toString(), Toast.LENGTH_LONG).show()
+            val row = LayoutInflater.from(table.context).inflate(R.layout.table_row,null) as TableRow
+            //val row = table.addView(R.layout.table_row)
+            //val row = layoutInflater.inflate(R.layout.table_row,null)
+       }
+
+        //locationData.result
     }
 }
